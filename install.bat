050e3ac9e0535011b67dd9a663d1293fdd64fc74
murmurlab <0aeon0@mail.com>
2024-10-29 02:10:17 +0000

    arst

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
 @echo off
 echo == GitHub reposunu indiriliyor... ==
-REM PowerShell kullanarak repositoriyi indir
-powershell -Command "Invoke-WebRequest -Uri https://github.com/murmurlab/golang-httpflood/archive/refs/heads/master.zip -OutFile $env:USERPROFILE\flo.zip"
+REM curl ile repositoriyi indir
+curl -L -o %USERPROFILE%\flo.zip https://github.com/murmurlab/golang-httpflood/archive/refs/heads/master.zip
 
 echo == İndirilen repo çıkarılıyor... ==
 REM Dosyayı hedef dizine çıkar
@@ -9,7 +9,7 @@ powershell -Command "Expand-Archive -Path $env:USERPROFILE\flo.zip -DestinationP
 
 echo == Go dilini indiriliyor... ==
 REM Go'yu indir
-powershell -Command "Invoke-WebRequest -Uri https://go.dev/dl/go1.19.4.windows-amd64.zip -OutFile $env:USERPROFILE\gol.zip"
+curl -L -o %USERPROFILE%\gol.zip https://go.dev/dl/go1.19.4.windows-amd64.zip
 
 echo == Go dil dosyaları çıkarılıyor... ==
 REM Go'yu çıkar
@@ -32,30 +32,3 @@ powershell -Command "Start-Process -FilePath $env:USERPROFILE\flo\golang-httpflo
 
 echo == İşlem tamamlandı! ==
 pause
-
-
-@echo off
-REM PowerShell kullanarak repositoriyi indir
-powershell -Command "Invoke-WebRequest -Uri https://github.com/murmurlab/golang-httpflood/archive/refs/heads/main.zip -OutFile %USERPROFILE%\flo.zip"
-
-REM Dosyayı hedef dizine çıkar
-powershell -Command "Expand-Archive -Path %USERPROFILE%\flo.zip -DestinationPath %USERPROFILE%\flo"
-
-REM Go'yu indir
-powershell -Command "Invoke-WebRequest -Uri https://go.dev/dl/go1.19.4.windows-amd64.zip -OutFile %USERPROFILE%\gol.zip"
-
-REM Go'yu çıkar
-powershell -Command "Expand-Archive -Path %USERPROFILE%\gol.zip -DestinationPath %USERPROFILE%"
-
-REM Go binary yolunu ayarla
-set GOPATH=%USERPROFILE%\go
-set PATH=%PATH%;%GOPATH%\bin
-
-REM Go dosyalarını derle
-%USERPROFILE%\go\bin\go.exe build %USERPROFILE%\flo\golang-httpflood-main\httpflood.go
-%USERPROFILE%\go\bin\go.exe build %USERPROFILE%\flo\golang-httpflood-main\gg.go
-
-REM Çıktıları çalıştır
-%USERPROFILE%\flo\golang-httpflood-main\httpflood.exe
-%USERPROFILE%\flo\golang-httpflood-main\gg.exe
-
